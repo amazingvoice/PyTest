063a0639ea0840442cafa19f558021260d8b7fa5
Qing Zhang <qz761@outlook.com>
2025-07-18 17:00:56 -0700

    3rd commit

--- pythonTest.py
+++ pythonTest.py
@@ -3,3 +3,4 @@ name = "qing"
 email = "qz761@outlook.com"
 print(f"Name: {name}, Email: {email}")
 print("This is a test file for Python SCM integration.")
+print("This file is used to test SCM features in VSCode.")
